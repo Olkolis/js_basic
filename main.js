cc234fb72125f3862758e39aa77516a35b61979b
Anton_Savkin <olkolis.one@gmail.com>
2024-04-10 00:12:51 +0300

    main.js зроблене дз 3

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
-alert('Hey, this is my first JS program');
+// let it = +prompt('Введіть число','')
 
-console.log('Hey, this is my first JS program');
+// if (it % 15 === 0){
+//     console.log('FizzBuzz')
+// } else if (it % 5 === 0){
+//     console.log('Buzz')
+// } else if (it % 3 === 0){
+//     console.log('Fizz')
+// };
+
+// let i = +prompt('Введіть рік','')
+
+// if (i % 4 === 0){
+//     console.log('Рік високосний')
+// } else {
+//     console.log('Рік не високосний')
+// }
+
+let i = +prompt('Введіть скільки вам років',);
+
+let j = i % 10;
+if (j == 1){
+    console.log(`Тобі ${i} рік`);
+} else if (j >= 2 && j <= 4){
+    console.log(`Тобі ${i} роки`);
+} else if (j >= 5 || j == 0){
+    console.log(`Тобі ${i} років`);
+}
+
